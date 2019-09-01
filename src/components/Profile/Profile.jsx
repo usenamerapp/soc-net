b1cf0f3c75795e108df3a0802db7e5b103881896
userapp <googgllebox@gmail.com>
2019-09-01 23:37:40 +0300

    15-структура папок

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import styles from './Profile.module.css';
+import Post from './Post/Post'
 
 const Profile = () => {
     return (
         <div className={styles.content}>
-            {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe9Zo33u5CGGntITUINySBjZFWNdEqbqRAbB4J70LVnBez0MuZ" /> */}
-            <div className={styles.item}>Post 1</div>
-            <div className={styles.item}>Post 2</div >
-            <div className={styles.item}>Post 3</div>
-            <div className={styles.item}>Post 4</div>
+           <Post />
         </div>)
 }
 
